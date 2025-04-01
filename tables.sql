e2a110b3b452283f6e491ab67abd67b814d7d227
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-01 15:08:44 +0530

    creating a table for the files

--- tables.sql
+++ tables.sql
@@ -1,2 +1,9 @@
+CREATE database if not exists myvcs;
 USE myvcs;
+
+create table if not exists files(
+    id int auto_increment primary key,
+    hash varchar(40) unique not null,
+    context text not null
+);
 
