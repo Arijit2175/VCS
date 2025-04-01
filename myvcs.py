26c77c6e317b5affb2a876ef4469ee15d2865c7e
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-01 15:25:11 +0530

    Connection of database

--- myvcs.py
+++ myvcs.py
@@ -1,3 +1,18 @@
 import mysql.connector
 from mysql.connector import Error
 
+def create_connection(host_name, user_name, user_password, db_name):
+    """Create a database connection to a MySQL database."""
+    conn = None
+    try:
+        conn = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            password=user_password,
+            database=db_name
+        )
+        if conn.is_connected():
+            print("Connection established")
+    except Error as e:
+        print(f"Error: '{e}'")
+    return conn
