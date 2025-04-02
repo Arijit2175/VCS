df26bb83d9641fd9a93d11d926a0ecfb9c5e621e
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-02 19:40:07 +0530

    made few changes in commits and branches tables

--- tables.sql
+++ tables.sql
@@ -8,12 +8,8 @@ CREATE TABLE IF NOT EXISTS files (
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS branches;
-CREATE TABLE IF NOT EXISTS branches (
-    name VARCHAR(50) PRIMARY KEY,
-    latest_commit VARCHAR(64)
-) ENGINE=InnoDB;
-
 DROP TABLE IF EXISTS commits;
+
 CREATE TABLE IF NOT EXISTS commits (
     id INT AUTO_INCREMENT PRIMARY KEY,
     commit_hash VARCHAR(64) UNIQUE NOT NULL,  
@@ -23,12 +19,17 @@ CREATE TABLE IF NOT EXISTS commits (
     branch_name VARCHAR(50) NOT NULL
 ) ENGINE=InnoDB;
 
-ALTER TABLE branches
-ADD FOREIGN KEY (latest_commit) REFERENCES commits(commit_hash) ON DELETE SET NULL;
+CREATE TABLE IF NOT EXISTS branches (
+    name VARCHAR(50) PRIMARY KEY,
+    latest_commit VARCHAR(64)
+) ENGINE=InnoDB;
 
 ALTER TABLE commits
 ADD FOREIGN KEY (parent_commit) REFERENCES commits(commit_hash) ON DELETE SET NULL,
 ADD FOREIGN KEY (branch_name) REFERENCES branches(name) ON DELETE CASCADE;
+
+ALTER TABLE branches
+ADD FOREIGN KEY (latest_commit) REFERENCES commits(commit_hash) ON DELETE SET NULL;
 
 CREATE INDEX idx_files_hash ON files(hash);
 CREATE INDEX idx_commits_branch_name ON commits(branch_name);
